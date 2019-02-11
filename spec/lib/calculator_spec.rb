c7ba60d84ae007c7c2c2f237dc45677ed47a6078
Paul Friedman <psfriedman@gmail.com>
2019-02-11 12:12:08 -0800

    additional test coverage
    for Calculator#total_pay based on rate type

--- spec/lib/calculator_spec.rb
+++ spec/lib/calculator_spec.rb
@@ -27,43 +27,135 @@ describe Calculator do
       expect(subject.total_pay).to be_an_instance_of(Integer)
     end
 
-    context "for one hours work" do
-      let(:start_time) { Time.parse("2019-01-01 5:00PM") }
-      let(:end_time)   { Time.parse("2019-01-01 6:00PM") }
+    context "Family A pays $15 per hour before 11pm, and $20 per hour the rest of the night" do
 
-      it "returns 15" do
-        expect(subject.total_pay).to eq 15
+      let(:rate_type) { "A" }
+
+      context "for one hours work" do
+        let(:start_time) { Time.parse("2019-01-01 5:00PM") }
+        let(:end_time)   { Time.parse("2019-01-01 6:00PM") }
+
+        it "returns 15" do
+          expect(subject.total_pay).to eq 15
+        end
+      end
+
+      context "for two hours work" do
+        let(:start_time) { Time.parse("2019-01-01 5:00PM") }
+        let(:end_time)   { Time.parse("2019-01-01 7:00PM") }
+
+        it "returns 30" do
+          expect(subject.total_pay).to eq 30
+        end
+      end
+
+      context "fractional hours" do
+        let(:start_time) { Time.parse("2019-01-01 5:00PM") }
+        let(:end_time)   { Time.parse("2019-01-01 6:30PM") }
+
+        it "returns pay for the full hour" do
+          expect(subject.total_pay).to eq 30
+        end
+      end
+
+      context "over switch-over hours" do
+
+        let(:start_time) { Time.parse("2019-01-01 10:00PM") }
+        let(:end_time)   { Time.parse("2019-01-02 12:30AM") }
+
+        it "handles changes in rates depending on hours worked" do
+          expect(subject.total_pay).to eq 55
+        end
+
       end
     end
 
-    context "for two hours work" do
-      let(:start_time) { Time.parse("2019-01-01 5:00PM") }
-      let(:end_time)   { Time.parse("2019-01-01 7:00PM") }
+    context "Family B pays $12 per hour before 10pm, $8 between 10 and 12, and $16 the rest of the night" do
 
-      it "returns 30" do
-        expect(subject.total_pay).to eq 30
+      let(:rate_type) { "B" }
+
+      context "for one hours work" do
+        let(:start_time) { Time.parse("2019-01-01 5:00PM") }
+        let(:end_time)   { Time.parse("2019-01-01 6:00PM") }
+
+        it "returns 12" do
+          expect(subject.total_pay).to eq 12
+        end
+      end
+
+      context "for two hours work" do
+        let(:start_time) { Time.parse("2019-01-01 5:00PM") }
+        let(:end_time)   { Time.parse("2019-01-01 7:00PM") }
+
+        it "returns 24" do
+          expect(subject.total_pay).to eq 24
+        end
+      end
+
+      context "fractional hours" do
+        let(:start_time) { Time.parse("2019-01-01 5:00PM") }
+        let(:end_time)   { Time.parse("2019-01-01 6:30PM") }
+
+        it "returns pay for the full hour" do
+          expect(subject.total_pay).to eq 24
+        end
+      end
+
+      context "over switch-over hours" do
+
+        let(:start_time) { Time.parse("2019-01-01 9:00PM") }
+        let(:end_time)   { Time.parse("2019-01-02 12:30AM") }
+
+        it "handles changes in rates depending on hours worked" do
+          expect(subject.total_pay).to eq 44
+        end
+
       end
     end
 
-    context "fractional hours" do
-      let(:start_time) { Time.parse("2019-01-01 5:00PM") }
-      let(:end_time)   { Time.parse("2019-01-01 6:30PM") }
+    context "Family C pays $21 per hour before 9pm, then $15 the rest of the night" do
 
-      it "returns pay for the full hour" do
-        expect(subject.total_pay).to eq 30
+      let(:rate_type) { "C" }
+
+      context "for one hours work" do
+        let(:start_time) { Time.parse("2019-01-01 5:00PM") }
+        let(:end_time)   { Time.parse("2019-01-01 6:00PM") }
+
+        it "returns 21" do
+          expect(subject.total_pay).to eq 21
+        end
+      end
+
+      context "for two hours work" do
+        let(:start_time) { Time.parse("2019-01-01 5:00PM") }
+        let(:end_time)   { Time.parse("2019-01-01 7:00PM") }
+
+        it "returns 42" do
+          expect(subject.total_pay).to eq 42
+        end
+      end
+
+      context "fractional hours" do
+        let(:start_time) { Time.parse("2019-01-01 5:00PM") }
+        let(:end_time)   { Time.parse("2019-01-01 6:30PM") }
+
+        it "returns pay for the full hour" do
+          expect(subject.total_pay).to eq 42
+        end
+      end
+
+      context "over switch-over hours" do
+
+        let(:start_time) { Time.parse("2019-01-01 8:00PM") }
+        let(:end_time)   { Time.parse("2019-01-02 12:30AM") }
+
+        it "handles changes in rates depending on hours worked" do
+          expect(subject.total_pay).to eq 81
+        end
+
       end
     end
 
-    context "over switch-over hours" do
-
-      let(:start_time) { Time.parse("2019-01-01 10:00PM") }
-      let(:end_time)   { Time.parse("2019-01-02 12:30AM") }
-
-      it "handles changes in rates depending on hours worked" do
-        expect(subject.total_pay).to eq 55
-      end
-
-    end
 
   end
 
