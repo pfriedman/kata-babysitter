14fb9c5eeab13e740127ca6b25275bd21ac75d76
Paul Friedman <psfriedman@gmail.com>
2019-02-11 10:52:24 -0800

    introduce Factory pattern
    for Rate class

--- lib/rate.rb
+++ lib/rate.rb
@@ -1,10 +1,39 @@
 class Rate
 
+  def initialize(type = "A")
+    @rate = Object.const_get("Rate#{type}").new
+  end
+
   def for_hour(time)
-    if time.hour >= 23 || time.hour <= 4 # after 11:00PM or before 4:00AM
+    @rate.for_hour(time)
+  end
+end
+
+# A pays $15 per hour before 11pm, and $20 per hour the rest of the night
+class RateA
+
+  def for_hour(time)
+    hour = time.hour
+    if hour >= 23 || hour <= 4
       20
     else
       15
+    end
+  end
+
+end
+
+# B pays $12 per hour before 10pm, $8 between 10 and 12, and $16 the rest of the night
+class RateB
+
+  def for_hour(time)
+    hour = time.hour
+    if hour >= 22 && hour <= 23
+      8
+    elsif hour >= 23 || hour <= 4
+      16
+    else
+      12
     end
   end
 
--- spec/lib/rate_spec.rb
+++ spec/lib/rate_spec.rb
@@ -2,16 +2,17 @@ require "spec_helper"
 
 describe Rate do
 
-  subject { described_class.new }
+  let(:rate_type) { "A" }
+
+  subject { described_class.new(rate_type) }
 
   describe "#new" do
-
     it { expect(subject).not_to be_nil }
-
   end
 
   describe "#for_hour" do
     context "Family A pays $15 per hour before 11pm, and $20 per hour the rest of the night" do
+      let(:rate_type) { "A" }
       it do
         [
           ["5:00PM", 15],
@@ -33,7 +34,25 @@ describe Rate do
     end
 
     context "Family B pays $12 per hour before 10pm, $8 between 10 and 12, and $16 the rest of the night" do
-
+      let(:rate_type) { "B" }
+      it do
+        [
+          ["5:00PM", 12],
+          ["6:00PM", 12],
+          ["7:00PM", 12],
+          ["8:00PM", 12],
+          ["9:00PM", 12],
+          ["10:00PM", 8],
+          ["11:00PM", 8],
+          ["12:00AM", 16],
+          ["1:00AM", 16],
+          ["2:00AM", 16],
+          ["3:00AM", 16],
+          ["4:00AM", 16],
+        ].each do |hour, rate|
+          expect(subject.for_hour(Time.parse("2019-01-01 #{hour}"))).to eq rate
+        end
+      end
     end
 
     context "Family C pays $21 per hour before 9pm, then $15 the rest of the night" do
